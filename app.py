e05fcb38f1dee63ff14fa89d4dfe3b61961106ed
sathyabharathi <sathyabharathi00131@gmail.com>
2024-08-08 19:35:08 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from spire.xls import *
+from spire.xls.common import *
+import os
+
+
+# Chanen based on your requirement
+inputFile = "/var/myapp/index.xlsx"
+outputFile = "/var/www/html/index.html"
+
+
+while True:
+    try:
+        # Create a Workbook instance
+        workbook = Workbook()
+
+        # Load a sample Excel file
+        workbook.LoadFromFile(inputFile)
+
+        # Get the first sheet of this file
+        sheet = workbook.Worksheets[0]
+
+        # Save the worksheet to HTML
+        sheet.SaveToHtml(outputFile)
+        workbook.Dispose()
+
+        os.remove("sample_1.xlsx")
+
+    except:
+        continue
